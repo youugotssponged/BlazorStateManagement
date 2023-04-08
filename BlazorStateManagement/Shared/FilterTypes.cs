180889d4b2897c2abb42d6d5430ccc4cd9c69058
Jordan <jordanmccann64@outlook.com>
2023-04-08 03:46:24 +0100

    Initial upload

---
+++ BlazorStateManagement/Shared/FilterTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorStateManagement.Shared
+{
+    public enum FilterTypes
+    {
+        DEFAULT = 0,
+        LOWEST_TO_HIGHEST,
+        HIGHEST_TO_LOWEST,
+        RANDOM,
+        JORDAN_WAS_HERE
+    }
+}
